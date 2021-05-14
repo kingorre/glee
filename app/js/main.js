5175667538f361a0250d37a9c129d17a8dfe3be0
kingorre <senpai_gud@ukr.net>
2021-05-14 21:33:24 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
+  
+  $('.shop-slider').slick({
+    dots: true,
+    arrows: false
+  });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,10 @@ function browsersync() {
 }
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src([
+    'app/scss/style.scss',
+    'node_modules/slick-carousel/slick/slick.scss'
+  ])
     .pipe(scss({outputStyle: 'expanded'}))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
@@ -31,6 +34,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.min.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
