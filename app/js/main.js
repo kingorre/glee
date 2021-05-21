d4bfb9585526773af810fcba82a81a72e0758df0
kingorre <senpai_gud@ukr.net>
2021-05-21 16:29:41 +0300

    added adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,44 @@
 $(function(){
 
+  var pos = 0;
+
+  $(window).scroll(function () {
+    var height = $(window).scrollTop();
+    var currentPos = $(this).scrollTop();
+    
+    if (height > 71) {
+      $('.header').addClass('header--bg');
+    } else {
+      $('.header').removeClass('header--bg');
+    };
+
+    if (currentPos > pos) {
+      $('.header').addClass('header--display');
+    } else {
+      $('.header').removeClass('header--display');
+    };
+    pos = currentPos;
+  });
+
+  $('.burger').on('click', function () {
+    $('.burger').toggleClass('burger--active');
+    $('.menu__list').toggleClass('menu__list--active');
+    $('body').toggleClass('lock');
+    $('.user-nav').removeClass('user-nav--active');
+  });
+
+  $('.menu__link, .logo').on('click', function () {
+    $('.menu__list').removeClass('menu__list--active');
+    $('body').removeClass('lock');
+    $('.user-nav').removeClass('user-nav--active');
+  });
+
+  $('.tools-btn').on('click', function () {
+    $('.user-nav').toggleClass('user-nav--active');
+    $('.tools-btn').toggleClass('tools-btn--active');
+  });
+
+
   $('.top-slider').slick({
     dots: true,
     arrows: false
@@ -12,7 +51,32 @@ $(function(){
     slidesToScroll: 1,
     autoplaySpeed: 2000,
     autoplay: true,
-    pauseOnHover: true
+    infinite: true,
+    pauseOnHover: true,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('.popup-youtube').magnificPopup({
