0e4e301dc386517a900e1b872e1a4589d4c94be8
kingorre <senpai_gud@ukr.net>
2021-06-08 11:15:20 +0300

    18 dz addec product details

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,123 @@
 $(function(){
 
+  $('.tabs__content').not('.tabs__content--active').hide();
+	$('.tabs__btn').on('click',function (e) {
 
+    $('.tabs__btn').removeClass('tabs__btn--active');
+    $(this).addClass('tabs__btn--active');
+
+		var tabItem = $(this),
+			tabContentItem = $('.tabs__content'),
+			tabItemPosition = tabItem.data('tab');
+
+		tabContentItem.filter('.tabs__content--' + tabItemPosition)
+			.fadeIn(700)
+			.siblings()
+			.hide();
+      
+    $(e.currentTarget)
+    	.addClass('tabs__content--active')
+      .siblings()
+      .removeClass('tabs__content--active');
+	});
+
+  $('.related-products__slider').slick({
+    prevArrow: '<button class="related-products__btn related-products__btn--left"><span class="sr-only">move to previous slide</span><img src="../images/icons/slider-left.svg"></button>',
+    nextArrow: '<button class="related-products__btn related-products__btn--right"><span class="sr-only">Move to next slide</span><img src="../images/icons/slider-right.svg"></button>',
+    dots: false,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1240,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.product-details__form').each(function() {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.product-details__button--plus'),
+      btnDown = spinner.find('.product-details__button--minus'),
+      min = input.attr('min');
+
+    btnUp.click(function() {
+      var oldValue = parseFloat(input.val());
+      
+        var newVal = oldValue + 1;
+      
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
+
+  $('.product-details__input').styler();
+
+  $(".product-details__star").rateYo({
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing   : "12px",
+    starWidth: "18px"
+  });
+
+  $('.product-details__small').slick({
+    asNavFor: '.product-details__big',
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    draggable: false,
+    vertical: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          vertical: false
+        }
+      },
+    ]
+  });
+
+  $('.product-details__big').slick({
+    asNavFor: '.product-details__small',
+    dots: false,
+    arrows: false,
+    slidesToShow: 1,
+    draggable: false,
+    fade: true
+  });
+  
   $('.side-bar__btn').on('click', function () {
     $(this).next().slideToggle(500);
     $(this).toggleClass('side-bar__btn--clicked');
